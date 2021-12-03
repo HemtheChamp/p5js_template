6a5a0594bc6da5107001a082efe88cebaa6ae35a
HemtheChamp <93389391+HemtheChamp@users.noreply.github.com>
2021-12-03 15:19:44 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,19 @@
+var ball={
+  x:20,
+  y:100,
+  r:30,
+  xspeed:0,
+  yspeed:0,
+  color:['purple','red','green','blue',]
+  
+
 function setup() {
+  background('black'); 
   createCanvas(400, 400);
+  circle(ball.x,ball.y,ball.r);
+  fill(ball.color[3]);
+  ball.xspeed=1;
+  ball.x=ball.x+ball.xspeed;
 }
 
 function draw() {
